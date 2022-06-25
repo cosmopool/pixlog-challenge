1d1ac4d99f2f7a964ca4c3a2dcb4b04c1b0c3df8
Kaio Delphino <dash.zet@gmail.com>
2022-06-25 15:59:30 -0300

    feat: implement remote datasource
    fix stub

---
+++ lib/app/modules/i18n_resources/external/remote_datasource.dart
@@ -0,0 +1,18 @@
+import 'package:pixlog_challenge/app/modules/core/services/http/http_client_interface.dart';
+import 'package:pixlog_challenge/app/modules/i18n_resources/domain/entity/resource_entity.dart';
+import 'package:pixlog_challenge/app/modules/i18n_resources/external/mappers/resources_mapper.dart';
+import 'package:pixlog_challenge/app/modules/i18n_resources/infra/interfaces/resources_datasource_interface.dart';
+
+class RemoteDatasource implements ResourcesDatasourceInterface {
+  final IHttpClientService _httpClient;
+  final url = "http://url";
+
+  RemoteDatasource(this._httpClient);
+
+  @override
+  Future<List<ResourceEntity>> fetch() async {
+    final response = await _httpClient.get(url);
+    final resources = ResourcesMapper.fromList(response.data);
+    return resources;
+  }
+}
---
+++ test/app/modules/i18n_resources/external/remote/remote_datasource_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:pixlog_challenge/app/modules/core/services/http/http_client_interface.dart';
+import 'package:pixlog_challenge/app/modules/core/services/models/http_response.dart';
+import 'package:pixlog_challenge/app/modules/i18n_resources/domain/entity/resource_entity.dart';
+import 'package:pixlog_challenge/app/modules/i18n_resources/external/remote_datasource.dart';
+import 'package:pixlog_challenge/app/modules/i18n_resources/infra/interfaces/resources_datasource_interface.dart';
+
+import '../../../../../stubs/resources_response.dart';
+
+
+class MockHttpClient extends Mock implements IHttpClientService {}
+
+late ResourcesDatasourceInterface datasource;
+late IHttpClientService httpClient;
+
+final httpResponse = HttpResponse(statusCode: 200, data: resourcesResponse);
+
+void main() async {
+  setUp(() async {
+    httpClient = MockHttpClient();
+    datasource = RemoteDatasource(httpClient);
+    when(() => httpClient.get(any())).thenAnswer((_) async => httpResponse);
+  });
+
+  test('Should return a list of resources', () async {
+    final response = await datasource.fetch();
+    expect(response.runtimeType, List<ResourceEntity>);
+  });
+
+  test('Should call client get method once', () async {
+    await datasource.fetch();
+    verify(() => httpClient.get(any())).called(1);
+  });
+}
