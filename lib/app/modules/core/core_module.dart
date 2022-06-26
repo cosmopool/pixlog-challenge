85f2d5c3be9ea102cdf79c7d81d177e2229c27aa
Kaio Delphino <dash.zet@gmail.com>
2022-06-26 15:31:44 -0300

    feature: add binds for core module

---
+++ lib/app/modules/core/core_module.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pixlog_challenge/app/modules/core/services/http/http_client.dart';
+
+class CoreModule extends Module {
+  final url = "http://portal.greenmilesoftware.com/get_resources_since";
+
+  @override
+  List<Bind> get binds => [
+        Bind.lazySingleton((i) => Dio(), export: true),
+        Bind.lazySingleton((i) => HttpClientService(i()), export: true),
+      ];
+}
