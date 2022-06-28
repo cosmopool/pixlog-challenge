ca55590ddd84f1bec685d2802e5a980cc982bffe
Kaio Delphino <dash.zet@gmail.com>
2022-06-27 22:53:35 -0300

    fix: stub method for cache

--- test/app/modules/i18n_resources/infra/repositories/resources_repository_test.dart
+++ test/app/modules/i18n_resources/infra/repositories/resources_repository_test.dart
@@ -2,19 +2,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:pixlog_challenge/app/modules/i18n_resources/domain/entity/resource_entity.dart';
 import 'package:pixlog_challenge/app/modules/i18n_resources/domain/interfaces/resources_repository_interface.dart';
-import 'package:pixlog_challenge/app/modules/i18n_resources/external/local/hive_datasource.dart';
+import 'package:pixlog_challenge/app/modules/i18n_resources/external/mappers/resources_mapper.dart';
 import 'package:pixlog_challenge/app/modules/i18n_resources/infra/interfaces/resources_datasource_interface.dart';
+import 'package:pixlog_challenge/app/modules/i18n_resources/infra/interfaces/resources_local_datasource_interface.dart';
 import 'package:pixlog_challenge/app/modules/i18n_resources/infra/repositories/resources_repository.dart';
 
-import '../../../../../stubs/entity_list.dart';
+import '../../../../../stubs/resources_response.dart';
 
 
 class MockResourcesDatasource extends Mock implements IResourcesDatasource {}
-class MockCacheDatasource extends Mock implements HiveDatasource {}
+class MockCacheDatasource extends Mock implements IResourcesLocalDatasource {}
 
 late IResourcesRepository repository;
 late IResourcesDatasource datasource;
-late HiveDatasource cache;
+late IResourcesLocalDatasource cache;
 
 void main() async {
   setUp(() async {
@@ -22,7 +23,8 @@ void main() async {
     cache = MockCacheDatasource();
     repository = ResourcesRepository(datasource, cache);
 
-    when(datasource.fetch).thenAnswer((_) async => entityList);
+    when(datasource.fetch).thenAnswer((_) async => ResourcesMapper.fromList(resourcesResponse));
+    when(() => cache.save(any())).thenAnswer((_) async {});
   });
 
   test('Should return a list of resources', () async {
